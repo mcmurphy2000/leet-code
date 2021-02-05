1b05c4e596a73b00ca2c25a9851e1a56785d85fe
Stan Natkovskyi <jmarton2000@gmail.com>
2021-02-05 15:50:45 +0200

    Modify longest-palindromic-substring to use recursion

--- longest-palindromic-substring/src/main/java/Solution.java
+++ longest-palindromic-substring/src/main/java/Solution.java
@@ -1,28 +1,45 @@
+class PalLength implements Comparable<PalLength> {
+    final int startIndex;
+    final int length;
+
+    public PalLength(int startIndex, int length) {
+        this.startIndex = startIndex;
+        this.length = length;
+    }
+
+    @Override
+    public int compareTo(PalLength other) {
+        return this.length - other.length;
+    }
+}
+
 public class Solution {
 
     public String longestPalindrome(String s) {
-        for (int strLen = s.length(); strLen > 1; strLen--) {
-            for (int startIndex = 0; startIndex <= s.length() - strLen; startIndex++) {
-                int endIndex = startIndex + strLen;
-                if (isPalindrome(s, startIndex, endIndex)) {
-                    return s.substring(startIndex, endIndex);
-                }
+        PalLength longest = new PalLength(0, 1);
+        for (int startIndex = 0; startIndex < s.length(); startIndex++) {
+            PalLength lengthOdd = palindromeLength(s, startIndex, startIndex + 1);
+            PalLength lengthEven = palindromeLength(s, startIndex, startIndex + 2);
+            if (lengthOdd != null && lengthOdd.compareTo(longest) > 0) {
+                longest = lengthOdd;
             }
-
+            if (lengthEven != null && lengthEven.compareTo(longest) > 0) {
+                longest = lengthEven;
+            }
         }
-        return s.substring(0, 1);
+        return s.substring(longest.startIndex, longest.startIndex + longest.length);
     }
 
     // endIndex exclusive
-    private boolean isPalindrome(String s, int startIndex, int endIndex) {
-        endIndex--;
-        while (startIndex < endIndex) {
-            if (s.charAt(startIndex) != s.charAt(endIndex)) {
-                return false;
-            }
-            startIndex++;
-            endIndex--;
+    private PalLength palindromeLength(String s, int startIndex, int endIndex) {
+        if (startIndex < 0 || endIndex > s.length()) return null;
+        if (s.charAt(startIndex) != s.charAt(endIndex - 1)) return null;
+        PalLength thisLength = new PalLength(startIndex, endIndex - startIndex);
+        PalLength biggerLength = palindromeLength(s, startIndex - 1, endIndex + 1);
+        if (biggerLength == null || thisLength.compareTo(biggerLength) >= 0) {
+            return thisLength;
+        } else {
+            return biggerLength;
         }
-        return true;
     }
 }
