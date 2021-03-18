9b5050c11275af6feb049aaf84003adcf858a07f
Stan Natkovskyi <jmarton2000@gmail.com>
2021-03-18 16:26:22 +0200

    Add best-time-to-buy-and-sell-stock-ii

---
+++ best-time-to-buy-and-sell-stock-ii/src/test/java/SolutionTests.java
@@ -0,0 +1,32 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTests {
+
+    private final Solution tested = new Solution();
+
+    @Test
+    public void shouldWork() {
+        int[] prices = {7,1,5,3,6,4};
+        int expected = 7;
+
+        assertEquals(expected, tested.maxProfit(prices));
+    }
+
+    @Test
+    public void shouldWork2() {
+        int[] prices = {1,2,3,4,5};
+        int expected = 4;
+
+        assertEquals(expected, tested.maxProfit(prices));
+    }
+
+    @Test
+    public void shouldWork3() {
+        int[] prices = {7,6,4,3,1};
+        int expected = 0;
+
+        assertEquals(expected, tested.maxProfit(prices));
+    }
+}
