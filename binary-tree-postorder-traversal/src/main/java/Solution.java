41cf134fcc9ab4d79fcd3c952d3c5dc1ae6b35e4
Stan Natkovskyi <stan@kyla.com>
2022-03-19 15:22:26 +0200

    Add binary-tree-postorder-traversal

---
+++ binary-tree-postorder-traversal/src/main/java/Solution.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        postorderTraversal(root, list);
+        return list;
+    }
+
+    private void postorderTraversal(TreeNode node, List<Integer> list) {
+        if (node == null) return;
+        postorderTraversal(node.left, list);
+        postorderTraversal(node.right, list);
+        list.add(node.val);
+    }
+}
