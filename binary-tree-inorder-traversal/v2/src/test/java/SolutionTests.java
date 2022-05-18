4efa163d36c0a866db753b8ba2fd75ff87a01c45
Stan Natkovskyi <stan@kyla.com>
2022-05-18 09:31:21 +0300

    Add binary-tree-inorder-traversal v2

---
+++ binary-tree-inorder-traversal/v2/src/test/java/SolutionTests.java
@@ -0,0 +1,47 @@
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTests {
+
+    private Solution tested = new Solution();
+
+    @Test
+    public void shouldWork1() {
+        /*
+                 1
+                  \
+                   2
+                  /
+                 3
+         */
+        TreeNode tn3 = new TreeNode(3);
+        TreeNode tn2 = new TreeNode(2, tn3, null);
+        TreeNode root = new TreeNode(1, null, tn2);
+
+        List<Integer> result = tested.inorderTraversal(root);
+        assertEquals(3, result.size());
+        assertEquals(Arrays.asList(1, 3, 2), result);
+    }
+
+    @Test
+    public void shouldWork2() {
+        List<Integer> result = tested.inorderTraversal(null);
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    public void shouldWork3() {
+        /*
+                 1
+         */
+        TreeNode root = new TreeNode(1);
+
+        List<Integer> result = tested.inorderTraversal(root);
+        assertEquals(1, result.size());
+        assertEquals(Arrays.asList(1), result);
+    }
+}
